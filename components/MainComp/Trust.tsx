df334c70879bcc3aed2e2a93495880c6568b4d8b
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-11-07 02:48:55 +0500

    done trust

--- components/MainComp/Trust.tsx
+++ components/MainComp/Trust.tsx
@@ -4,11 +4,11 @@ import React from 'react'
 
 const Trust = () => {
   return (
-    <section className='flex items-center justify-center gap-10 my-16'>
+    <section className='sm:grid  w-full sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 max-w-7xl mx-auto gap-10 my-16 place-content-center flex flex-col justify-center items-center'>
       {trustData.map((item, i) => (
-        <div key={i} className='text-center gap-2 flex items-center flex-col w-[250px]'>
-            <item.icon className='text-4xl' />
-            <p className={`${cormorant.className} text-[22px] font-semibold`}>{item.heading}</p>
+        <div key={i} className='text-center gap-2 flex items-center flex-col justify-center w-[250px] px-10 sm:px-0'>
+            <item.icon className='text-3xl md:text-4xl' />
+            <p className={`${cormorant.className} text-[20px] md:text-[22px] font-semibold`}>{item.heading}</p>
             <p className='text-sm'>{item.desc}</p>
         </div>
       ))}
