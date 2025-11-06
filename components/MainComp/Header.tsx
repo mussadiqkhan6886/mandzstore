3b0a1b95f47dcd85a373251e9ee3360f130519a1
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-11-06 12:41:17 +0500

    starting with header

---
+++ components/MainComp/Header.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+      <div>
+
+      </div>
+      <div>
+        <div>
+
+        </div>
+        <div>
+
+        </div>
+        <div>
+            
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
