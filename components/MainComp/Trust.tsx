e3f0939943eee3af352194664c2fff76073c64cd
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-11-06 16:10:18 +0500

    making trust

---
+++ components/MainComp/Trust.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Trust = () => {
+  return (
+    <section>
+      
+    </section>
+  )
+}
+
+export default Trust
--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -1,5 +1,31 @@
+import { PackageCheck, Truck, HandCoins, Sparkles } from "lucide-react";
+
 export const upperHeaderData = [
     "Free Shipping above RS 4500",
     "Hassle-Free Returns",
     "15000+ Orders, 1000+ 5⭐ Reviews"
 ]
+
+
+export const trustData = [
+  {
+    icon: PackageCheck,
+    heading: "150,000+ Orders Delivered",
+    desc: "Trusted by modest wear lovers worldwide.",
+  },
+  {
+    icon: Truck,
+    heading: "Fast Nationwide Delivery",
+    desc: "Swift Delivery. Seamless Support. Tailored for You.",
+  },
+  {
+    icon: HandCoins,
+    heading: "COD Available",
+    desc: "Secure your order now and pay when it arrives.",
+  },
+  {
+    icon: Sparkles,
+    heading: "Premium Quality",
+    desc: "From fit to fabric, your voice shapes everything we create.",
+  },
+];
