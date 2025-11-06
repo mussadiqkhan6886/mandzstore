cff23988599f5acb78ad148a5c5540d979db9dae
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-11-07 00:55:28 +0500

    dine vision now reviews

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -2,6 +2,7 @@ import ByCollection from '@/components/MainComp/ByCollection'
 import Gift from '@/components/MainComp/Gift'
 import Hero from '@/components/MainComp/Hero'
 import Trust from '@/components/MainComp/Trust'
+import Vision from '@/components/MainComp/Vision'
 import { byCollection, essentials } from '@/lib/constants'
 import React from 'react'
 
@@ -13,6 +14,7 @@ const Home = () => {
       <ByCollection data={byCollection} header='shop by collection' />
       <Gift />
       <ByCollection data={essentials} header='hijab essentials' />
+      <Vision />
     </main>
   )
 }
--- components/MainComp/Vision.tsx
+++ components/MainComp/Vision.tsx
@@ -1,9 +1,22 @@
+import { cormorant } from '@/lib/fonts'
+import Image from 'next/image'
 import React from 'react'
 
 const Vision = () => {
   return (
-    <section>
-      
+    <section className='flex my-20 gap-26'>
+        <div className='flex-1'>
+            <Image src={"/vision.png"} alt='gift image' width={1000} height={1000} className='w-full h-full scale-115' />
+        </div>
+        <div className='flex flex-1 justify-center flex-col pr-20 bg-zinc-100 gap-6'>
+            <h4 className={`${cormorant.className} text-4xl mb-3`}>A SHARED VISION</h4>
+            <p className='text-gray-800'>KEF began with two stories woven by intention.</p>
+            <p className=' text-gray-800 mb-3'>One of us had just embraced the hijab, searching for elegance and ease. The other, during lockdown, could not find hijabs that matched her style — so we created our own.
+            <br />
+            <br />
+            What started as a personal pursuit became a brand trusted by thousands. Today, KEF continues with that same vision: refined, breathable, and made for women who expect more.
+            </p>
+        </div>
     </section>
   )
 }
