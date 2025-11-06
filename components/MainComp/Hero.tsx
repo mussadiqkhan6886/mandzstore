b4dcc7f891f88c4705e4a13fa7e6ffce91525c11
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-11-07 02:40:29 +0500

    heri section done resp

--- components/MainComp/Hero.tsx
+++ components/MainComp/Hero.tsx
@@ -5,10 +5,13 @@ import React from 'react'
 const Hero = () => {
   return (
     <section>
-      <Image src={"/main_hero_large.jpg"} alt='main banner hero image large screen' width={1000} height={1000} className='w-full h-full' />
-      <div className='flex flex-col items-center justify-center gap-7 py-9'>
-        <h1 className={`${cormorant.className} uppercase text-4xl`}>for women like you</h1>
-        <p className='text-gray-800 text-sm'>M&Z Store is more than modest fashion — it’s a community built on shared values. We create for women who choose elegance over excess, intention over impulse.</p>
+      <picture>
+        <source media="(max-width: 767px)" srcSet="/main_hero_small.jpg" />
+         <Image src={"/main_hero_large.jpg"} alt='main banner hero image large screen' width={1000} height={1000} className='w-full h-full' />
+    </picture>
+      <div className='flex flex-col items-center justify-center gap-4 md:gap-7 py-9'>
+        <h1 className={`${cormorant.className} uppercase text-3xl md:text-4xl`}>for women like you</h1>
+        <p className='text-gray-800 text-sm text-center px-4 md:px-0 '>M&Z Store is more than modest fashion — it’s a community built on shared values. We create for women who choose elegance over excess, intention over impulse.</p>
       </div>
       <Image src={"/second_hero_image_large.jpg"} alt='second banner hero image large screen' width={1000} height={1000} className='w-full h-full' />
     </section>
