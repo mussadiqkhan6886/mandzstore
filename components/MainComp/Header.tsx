4aed2fc7fad6d28ea26c81d4a827a6f9693ca1ef
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-11-06 15:59:26 +0500

    starting main

--- components/MainComp/Header.tsx
+++ components/MainComp/Header.tsx
@@ -1,27 +1,45 @@
 'use client';
 
-import React from 'react'
-import {Swiper} from "swiper/react"
+import React from 'react';
+import { upperHeaderData } from '@/lib/constants';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from 'swiper/modules';
+import {FiMenu, FiSearch, FiShoppingCart} from "react-icons/fi"
+
+import 'swiper/css';
 
 const Header = () => {
   return (
-    <header>
-      <Swiper>
-
+    <header >
+      <Swiper
+        modules={[Autoplay]}
+        autoplay={{ delay: 2500, disableOnInteraction: false }}
+        loop
+        className="text-center bg-[#EEECE5] text-[12px] py-2"
+      >
+        {upperHeaderData.map((item, i) => (
+          <SwiperSlide key={i}>
+            <p className='py-2 font-semibold uppercase tracking-[0.2em]'>{item}</p>
+          </SwiperSlide>
+        ))}
       </Swiper>
-      <div>
-        <div>
 
+      <div className="flex justify-between items-center py-6 px-12">
+        <button className="text-2xl">
+          <FiMenu />
+        </button>
+
+        <div >
+          <h1 className='font-bold text-4xl'>MZ STORE</h1>
         </div>
-        <div>
 
-        </div>
-        <div>
-
+        <div className="flex items-center space-x-4">
+          <button><FiSearch className='text-2xl' /></button>
+          <button><FiShoppingCart className='text-2xl' /></button>
         </div>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
---
+++ lib/constants/index.ts
@@ -0,0 +1,5 @@
+export const upperHeaderData = [
+    "Free Shipping above RS 4500",
+    "Hassle-Free Returns",
+    "15000+ Orders, 1000+ 5⭐ Reviews"
+]
