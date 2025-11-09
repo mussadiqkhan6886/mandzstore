174aeeb7fc43aea40c3e18e93f76048fdae72d17
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-11-09 22:18:33 +0500

    done products

--- app/(main)/collections/[slug]/page.tsx
+++ app/(main)/collections/[slug]/page.tsx
@@ -10,11 +10,14 @@ const SingleCollection = async ({params}: {params: Promise<{slug:string}>}) => {
   const data = collectionsData.filter(item => item.slug === slug)
   console.log(data)
   return (
-    <main className='max-w-7xl mx-auto'>
+    <main className='max-w-7xl mx-auto my-16'>
       {data.map(item => (
         <section key={item.title}>
         <HeaderProduct title={item.title} desc={item.desc} />
-        <p> Disclaimer: Colour may slightly differ from the actual picture, due to lighting and the device being used to view it. (Read: COLOUR DISCLAIMER)</p>
+        <p className='mb-10 font-gray-900 italic text-sm'> <span className='font-semibold'>Disclaimer:</span> Colour may slightly differ from the actual picture, due to lighting and the device being used to view it. (Read: COLOUR DISCLAIMER)</p>
+        <div className='mb-10'>
+          <p>{data.map(item => item.products.length)[0]} Products</p>
+        </div>
         <section className='grid grid-cols-3 gap-10'>
           {item.products.map(product => (
             <ProductCard key={product.id} {...product} slug={slug} />
