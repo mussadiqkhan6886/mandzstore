f1d02a43204be9db27a34226b2a5adf4c629a475
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-11-13 13:09:17 +0500

    compeleted

--- components/MainComp/ImageProduct.tsx
+++ components/MainComp/ImageProduct.tsx
@@ -27,7 +27,7 @@ const ImageProduct = ({images, name}: {images: string[], name: string}) => {
             ))}
           </div>
 
-          <div className="w-full md:w-[440px] h-full border border-gray-200 overflow-hidden">
+          <div className="w-full mb-2 md:w-[440px] h-full border border-gray-200 overflow-hidden">
             <Image
               src={mainImage}
               alt={name}
@@ -35,6 +35,25 @@ const ImageProduct = ({images, name}: {images: string[], name: string}) => {
               height={400}
               className="object-cover w-full h-full"
             />
+          </div>
+          <div className="flex flex-row gap-2">
+            {images.map((img, i) => (
+              <div
+                key={i}
+                className={`w-20 h-24 border cursor-pointer  overflow-hidden ${
+                  mainImage === img ? 'border-black' : 'border-gray-300'
+                }`}
+                onClick={() => setMainImage(img)}
+              >
+                <Image
+                  src={img}
+                  alt={`thumbnail ${i}`}
+                  width={80}
+                  height={80}
+                  className="object-cover w-full h-full"
+                />
+              </div>
+            ))}
           </div>
         </div>
   )
